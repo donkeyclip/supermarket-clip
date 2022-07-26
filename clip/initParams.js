91f47a0875ca93c7c7918b2762ff78eb16dab655
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2022-07-26 10:07:11 +0300

    fix: change path for assets

--- clip/initParams.js
+++ clip/initParams.js
@@ -7,56 +7,56 @@ export default [
         titleFontColor:"#2a881e",
       },
       company:{
-        logo:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/logo.png",
+        logo:"https://donkeyclip.github.io/supermarket-clip/server/assets/logo.png",
         title:"SUPERMARKETIO",
         slogan:"It's all about your needs.",
       },
       intro:{
         titleFontSize:"20px",
         sloganFontSize:"18px",
-        image1:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/image1.jpg",
-        image2:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/image2.jpg"
+        image1:"https://donkeyclip.github.io/supermarket-clip/server/assets/image1.jpg",
+        image2:"https://donkeyclip.github.io/supermarket-clip/server/assets/image2.jpg"
       },
       products:[
         {
           productName:"Eggs",
           productDescription:"Organic Free-Range Eggs,6 pcs",
-          productImage:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/eggs.png",
+          productImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/eggs.png",
           offer:"-40%",
           date:"Monday 13/6 - Saturday 18/6",
-          backgroundImage:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/eggs-back.jpg"
+          backgroundImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/eggs-back.jpg"
         },
         {
           productName:"Chicken",
           productDescription:"Organic chicken per kilo",
-          productImage:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/chicken.png",
+          productImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/chicken.png",
           offer:"3.50€",
           date:"Monday 13/6 - Saturday 18/6",
-          backgroundImage:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/chicken-back.jpg"
+          backgroundImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/chicken-back.jpg"
         },
         {
           productName:"Rice",
           productDescription:"Rice 1kg",
-          productImage:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/rice.png",
+          productImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/rice.png",
           offer:"1 + 1",
           date:"Monday 13/6 - Saturday 18/6",
-          backgroundImage:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/rice-back.jpg"
+          backgroundImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/rice-back.jpg"
         },
         {
           productName:"Pineapple",
           productDescription:"Pineapple per kilo",
-          productImage:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/pineapple.png",
+          productImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/pineapple.png",
           offer:"1.49€",
           date:"Monday 13/6 - Saturday 18/6",
-          backgroundImage:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/pineapple-back.jpg"
+          backgroundImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/pineapple-back.jpg"
         },
         {
           productName:"Bucket",
           productDescription:"Plastic bucket with handle 12 liters",
-          productImage:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/bucket.png",
+          productImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/bucket.png",
           offer:"-25%",
           date:"Monday 13/6 - Saturday 18/6",
-          backgroundImage:"https://elisavetampatzidou.github.io/supermarket-clip/server/assets/bucket-back.png"
+          backgroundImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/bucket-back.png"
         }
       ]
     },
