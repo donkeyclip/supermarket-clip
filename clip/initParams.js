72be1c9c47109dcd99959528985b22233845b382
Elisavet <68596273+ElisavetAmpatzidou@users.noreply.github.com>
2022-08-01 22:02:55 +0300

    Update initParams.js

--- clip/initParams.js
+++ clip/initParams.js
@@ -38,7 +38,7 @@ export default [
           productName:"Rice",
           productDescription:"Rice 1kg",
           productImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/rice.png",
-          offer:"1 + 1",
+          offer:"2 for 2€",
           date:"Monday 13/6 - Saturday 18/6",
           backgroundImage:"https://donkeyclip.github.io/supermarket-clip/server/assets/rice-back.jpg"
         },
